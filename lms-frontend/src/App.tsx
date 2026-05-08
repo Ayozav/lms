0fa825110555e0024953cd3077c76a82b8f4bba5
dofkee <d.osherovskaya@yandex.ru>
2026-05-08 17:55:38 +0300

    ничья и примерение с material.ui

--- lms-frontend/src/App.tsx
+++ lms-frontend/src/App.tsx
@@ -1,13 +1,17 @@
-// import React from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-// import App_styles from './App_styles.css'
+import './App_styles.css'; 
 
 function App() {
   return (
     <div>
-      <Button variant="contained">Кнопка</Button>
-      <TextField variant="filled" label="Введите текст" />
+      <Button variant="contained" size="medium" color="primary">Привет я кнопка</Button>
+      <Button variant="outlined" color="secondary" size="large">Привет я тоже кнопка, но с подвохом</Button> 
+      <Button variant="contained" className='im-still-want-custom-button'>А я абортыш</Button>
+'
+
+      <TextField variant="filled" label="Введите хоть что-то"/>
     </div>
   );
 }
